da49abfea63c840414ef2eff88d9e26db8fd63b9
syshim77 <syshim77@gmail.com>
2024-12-09 22:10:16 +0900

    [feature/evaluation_metrics] Add a function for computing extra metrics. This function calculates BLEU and ROUGE scores for each instance. The function has two outputs: one dictionary containing all scores calculated per instance, one dictionary containing average scores. Each output is used to evaluate the label of individual instance as well as the quality of the model (i.e., model that generates translated_text data).

--- manager/metrics_manager.py
+++ manager/metrics_manager.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
 from utils.metrics import accuracy, precision, recall, f1, bleu_score, rouge_score
+from utils.helpers import calculate_avg
 
 class MetricsManager:
     def __init__(self, enable_bleu_rouge: bool = False):
@@ -16,4 +17,34 @@ class MetricsManager:
             "precision": round(precision(y_true, y_pred, average=average, pos_label=pos_label), 2),
             "recall": round(recall(y_true, y_pred, average=average, pos_label=pos_label), 2),
             "f1-score": round(f1(y_true, y_pred, average=average, pos_label=pos_label), 2),
+        }
+    
+    
+    def compute_extra_metrics(self, references: List[str], translations: List[str]) -> (Dict[str, List[float]], Dict[str, float]):
+        if len(references) == 0 or len(translations) == 0:
+            raise ValueError("Reference and translated texts must not be empty.")
+        if len(references) != len(translations):
+            raise ValueError("Reference and translated texts must have the same length.")
+        
+        bleu_scores, rouge_1_scores, rouge_2_scores, rouge_l_scores = [], [], [], []
+        for ref, trans in zip(references, translations):
+            # Calculate BLEU score
+            bleu_scores.append(bleu_score(ref, trans))
+            
+            # Calculate ROUGE scores
+            rouge = rouge_score(ref, trans)
+            rouge_1_scores.append(rouge["ROUGE-1"])
+            rouge_2_scores.append(rouge["ROUGE-2"])
+            rouge_l_scores.append(rouge["ROUGE-L"])
+
+        return {
+            "bleu": bleu_scores,
+            "rouge_1": rouge_1_scores,
+            "rouge_2": rouge_2_scores,
+            "rouge_l": rouge_l_scores,
+        }, {
+            "BLEU": calculate_avg(bleu_scores),
+            "ROUGE-1": calculate_avg(rouge_1_scores),
+            "ROUGE-2": calculate_avg(rouge_2_scores),
+            "ROUGE-L": calculate_avg(rouge_l_scores),
         }
