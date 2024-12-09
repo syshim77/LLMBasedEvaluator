8e67713cff9488f0d3a633b47f57bdb01e1731fe
syshim77 <syshim77@gmail.com>
2024-12-09 22:31:43 +0900

    [feature/review_evaluator] Implement get_label_co nfidence function. Only the necessary parts are extracted from the generated text by this function. Get_label_confidence function utilizes find_pattern function in th e helpers.py file.

--- evaluators/review.py
+++ evaluators/review.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import List, Dict
+from utils.helpers import find_pattern, calculate_avg
 from evaluators.base import LLMBasedEvaluator
 
 class SentimentReviewEvaluator(LLMBasedEvaluator):
@@ -28,3 +29,30 @@ class SentimentReviewEvaluator(LLMBasedEvaluator):
         
         except Exception as e:
             raise Exception(f"An error occurred during evaluation: {e}")
+    
+    
+    def get_label_confidence(self, pattern: str, source_text: str) -> (str, float):
+        try:
+            match = find_pattern(pattern, source_text)
+            if match:
+                if len(match.groups()) == 2:
+                    label = match.group(1).lower()
+                    confidence = float(match.group(2))
+                elif float(match.group(1)):  # only confidence score is generated
+                    label = None
+                    confidence = float(match.group(1))
+                elif match.group(1).lower in ('positive', 'negative'): # only label is generated
+                    label = match.group(1).lower()
+                    confidence = 0.0
+                else:
+                    label, confidence = None, 0.0
+            else:
+                label, confidence = None, 0.0
+                print("No valid pattern found in the source text.")
+
+            return label, confidence
+
+        except ValueError as ve:
+            raise ve
+        except Exception as e:
+            raise Exception(f"An unexpected error occurred during label and confidence extraction: {e}")
